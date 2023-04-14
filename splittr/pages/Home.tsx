d9d79b0d07929e9fd50a7c43e1d3334df7a93a2d
Gage Harmon <gaharmon1@gmail.com>
2023-04-14 14:58:03 -0600

    testing API

--- splittr/pages/Home.tsx
+++ splittr/pages/Home.tsx
@@ -4,11 +4,11 @@ export default function Home() {
   const [data, setData] = useState([]);
 
   useEffect(() => {
-    fetch('http://localhost:5000/data')
+    fetch('http://localhost:3000/splittr')
       .then(response => response.json())
       .then(data => setData(data));
   }, []);
-
+console.log(data)
   return (
     <div>
       <h1>Hello, world!</h1>
