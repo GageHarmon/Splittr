0d543c7de0e2f22f9fb83bd26b55224d2af90b6c
Gage Harmon <gaharmon1@gmail.com>
2023-04-14 15:32:32 -0600

    WE GOT TABLES

---
+++ splittr/server/app.py
@@ -0,0 +1,20 @@
+from flask import Flask, make_response, request, jsonify
+from flask_migrate import Migrate
+from flask_restful import Api, Resource, fields, marshal_with
+
+from models import db, Users
+
+app = Flask( __name__ )
+app.config[ 'SQLALCHEMY_DATABASE_URI' ] = 'sqlite:///splittr.db'
+app.config[ 'SQLALCHEMY_TRACK_MODIFICATIONS' ] = False
+app.json.compact = False
+
+migrate = Migrate( app, db )
+
+db.init_app( app )
+
+api = Api(app)
+
+
+if __name__ == '__main__':
+    app.run()
