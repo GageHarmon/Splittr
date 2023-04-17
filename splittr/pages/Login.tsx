0eaa5402b5ad25ba46c7f7d6a0cbf2f3e338a4f9
Gage Harmon <gaharmon1@gmail.com>
2023-04-17 11:28:23 -0600

    added login.tsx

---
+++ splittr/pages/Login.tsx
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+const Login: React.FC = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+
+  const handleLogin = async () => {
+    try {
+      const response = await axios.post('http://localhost:5000/login', {
+        username,
+        password,
+      });
+      const data = response.data;
+
+      if (data.status === 'success') {
+        // Handle successful login (e.g., store the user ID and redirect)
+      } else {
+        setError('Invalid credentials');
+      }
+    } catch (error) {
+      setError('An error occurred');
+    }
+  };
+
+  return (
+    <div>
+      <h1>Login</h1>
+      <input
+        type="text"
+        placeholder="Username"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button onClick={handleLogin}>Login</button>
+      {error && <p>{error}</p>}
+    </div>
+  );
+};
+
+export default Login;
