e0f8177b1a04afda9398c642ae678a5dfc3a8ed7
Dylan Abbott <dylanabbott33@yahoo.com>
2023-04-19 16:07:25 -0600

    Myyyyyyyyyy betch

--- splittr/pages/Account.tsx
+++ splittr/pages/Account.tsx
@@ -1,111 +1,49 @@
-import React, { useState, useEffect } from 'react';
-import { CardElement, Elements, useStripe, useElements } from '@stripe/react-stripe-js';
-import { loadStripe } from '@stripe/stripe-js';
+import { useState } from 'react';
+// import Account from '/Account';
+// import './styles.css';
 
-export default function User({ users, userId, bills }) {
-  const [user, setUser] = useState({});
+export default function UserActivity({ bills, currUser }) {
   const [userBills, setUserBills] = useState([]);
-  const [selectedBill, setSelectedBill] = useState(null);
-  const [isPaying, setIsPaying] = useState(false);
-  const [paymentError, setPaymentError] = useState(null);
-  const [paymentSuccess, setPaymentSuccess] = useState(false);
+  // console.log(currUser.bill_users)
 
-  const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY);
+  if (!currUser) {
+    return <div>Loading.. </div>;
+  }
+  const filteredBills = bills.filter(bill => {
+    return currUser.bill_users.some(billUser => billUser.bill_id === bill.id);
+  });
 
-  useEffect(() => {
-    if (users) {
-      setUser(users.find(u => u.id === userId) || {});
-    }
-  }, [users, userId]);
-
-  useEffect(() => {
-    if (user && bills) {
-      const userBills = bills.filter(bill => bill.user_id === userId);
-      setUserBills(userBills);
-    }
-  }, [bills, user, userId]);
-
-  // const stripe = useStripe();
-  // const elements = useElements();
-
-  const handlePay = async () => {
-    setIsPaying(true);
-    setPaymentError(null);
-    setPaymentSuccess(false);
-
-    const { error, paymentMethod } = await stripe.createPaymentMethod({
-      type: 'card',
-      card: elements.getElement(CardElement),
-    });
-
-    if (error) {
-      console.error(error);
-      setPaymentError(error.message);
-      setIsPaying(false);
-    } else {
-      console.log(paymentMethod);
-
-      const selectedBillId = selectedBill.id;
-      const response = await fetch('/api/pay-bill', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          selectedBillId,
-          paymentMethodId: paymentMethod.id,
-        }),
-      });
-
-      const { error, success } = await response.json();
-
-      if (error) {
-        console.error(error);
-        setPaymentError(error.message);
-      } else {
-        console.log(success);
-        setPaymentSuccess(true);
-        setSelectedBill(null);
-      }
-
-      setIsPaying(false);
-    }
-  };
+  console.log(filteredBills);
 
   return (
-    <div>
-      <h1>Hello, {user.username}!</h1>
-      <h2>{user.username}'s Bills:</h2>
-      <ul>
-        {userBills.map(bill => (
-          <li key={bill.id}>
-            {bill.description}: {bill.amount}
-            <button disabled={isPaying} onClick={() => setSelectedBill(bill)}>
-              Pay Now
-            </button>
-          </li>
-        ))}
-      </ul>
-      {selectedBill && (
-        <div>
-          <h3>Pay {selectedBill.description}</h3>
-          <Elements stripe={stripePromise}>
-            <form onSubmit={handlePay}>
-              <label>
-                Card details
-                <CardElement />
-              </label>
-              <button type="submit" disabled={isPaying}>
-                {isPaying ? 'Paying...' : 'Pay Now'}
-              </button>
-              {paymentError && <p style={{ color: 'red' }}>{paymentError}</p>}
-              {paymentSuccess && <p style={{ color: 'green' }}>Payment successful!</p>}
-            </form>
-          </Elements>
-        </div>
-      )}
-
+    <div className="user-activity">
+      <h2>{currUser.username}'s Bills:</h2>
+      <div>
+        <p>Hello World!</p>
+        <p>
+          {currUser.username} has {filteredBills.length} bills.
+        </p>
+        <ul>
+          {filteredBills.map(bill => (
+            <li key={bill.id}>
+              <div>
+                <p>Bill Total: {bill.total_amount}</p>
+                <ul>
+                  {bill.bill_items.map(item => (
+                    <li key={item.id}>
+                      <div>
+                        <p>Title: {item.item.title} </p>
+                        <p>Description: {item.item.description}</p>
+                        <p>Price: {item.item.price}</p>
+                      </div>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
-
