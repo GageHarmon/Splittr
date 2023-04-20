895d291be6f8b4dc72bc09b80fa71705b8bcd3c3
Gage Harmon <gaharmon1@gmail.com>
2023-04-20 10:49:55 -0600

    adsf

--- splittr/pages/Group.tsx
+++ splittr/pages/Group.tsx
@@ -13,10 +13,16 @@ export default function Group({ bills, users, currUser }) {
     return bills.filter(singlebill => singlebill.id === bill.bill_id)[0]
   })
 
+  const [showForm, setShowForm] = useState(false);
+
+  const toggleForm = () => {
+    setShowForm(!showForm);
+  };
+
   return (
     <div className="min-h-screen bg-gradient-to-br from-rblue to-rorange flex flex-col justify-start items-center pt-12">
       <div className="bg-white p-6 mb-8 rounded-lg shadow-2xl">
-        <h2 className="text-3xl font-bold text-dblue">{currUser.username}'s Groups:</h2>
+        <h2 className="text-3xl font-bold text-dblue">{currUser.username}'s Groups</h2>
       </div>
 
       {/* +++++ USER GROUP SECTION +++++ */}
@@ -43,7 +49,41 @@ export default function Group({ bills, users, currUser }) {
             </li>
           ))}
         </ul>
+        <button
+          className="bg-rblue text-white font-bold py-2 px-4 rounded mt-4"onClick={toggleForm}>
+          Create Group
+        </button>
+
+        {showForm && (
+          <form className="mt-4">
+            <div className="mb-4">
+              <label htmlFor="groupName" className="block text-dblue font-bold mb-2">
+                Group Name:
+              </label>
+              <input
+                type="text"
+                id="groupName"
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-dblue leading-tight focus:outline-none focus:shadow-outline"
+                placeholder="Enter group name"
+              />
+            </div>
+            {/* Add more form fields as needed */}
+            <button
+              type="submit"
+              className="bg-rblue text-white font-bold py-2 px-4 rounded"
+            >
+              Submit
+            </button>
+          </form>
+        )}
       </div>
+      {/* <div className="bg-white p-6 mb-8 rounded-lg shadow-2xl">
+        <form >
+          <label htmlFor="group">Create a Group</label>
+          <input type="text" name="group" id="group" />
+          <button type="submit">Submit</button>
+        </form>
+      </div> */}
       <Footer activeLink={value} onChange={setValue} />
     </div>
   );
